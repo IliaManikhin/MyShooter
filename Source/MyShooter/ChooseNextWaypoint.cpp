ca574014fbb47ffafcccb57ea6eb2100308636f3
Ilia Manikhin <begbeder2012@yandex.ru>
2021-11-14 23:12:46 +0800

    Reading Behavior tree task from cpp

--- Source/MyShooter/ChooseNextWaypoint.cpp
+++ Source/MyShooter/ChooseNextWaypoint.cpp
@@ -2,10 +2,15 @@
 
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("What is going on?"))
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	UE_LOG(LogTemp, Warning, TEXT("the first index is : %i"), Index)
 		
 	return EBTNodeResult::Succeeded;
 }
