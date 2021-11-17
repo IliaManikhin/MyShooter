5c3b601f9b3d489ab1e7164f777a9c280e164767
Ilia Manikhin <begbeder2012@yandex.ru>
2021-11-17 19:27:47 +0800

    Big Refactor

--- Source/MyShooter/MyShooterGameMode.cpp
+++ Source/MyShooter/MyShooterGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "MyShooterGameMode.h"
 #include "MyShooterHUD.h"
-#include "MyShooterCharacter.h"
+#include "FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AMyShooterGameMode::AMyShooterGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behavior/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
